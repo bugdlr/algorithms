e2754658eb8e054c3320ab985dcd50a27f5296d8
James Cool <james.cool@thecontrolgroup.com>
2018-08-26 09:23:56 -0700

    formatting

--- chunkymonkey.js
+++ chunkymonkey.js
@@ -1,13 +1,14 @@
 // Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
 function chunkArrayInGroups(arr, size) {
-  // Break it up.
+    // Break it up.
     let array = [];
     let spliced;
     for (let i = 0; i < arr.length; i + size) {
-     let spliced = arr.splice(i, size);
-     array.push(spliced);
-  } return array;
+      spliced = arr.splice(i, size);
+      array.push(spliced);
+    }
+    return array;
 }
 
 
