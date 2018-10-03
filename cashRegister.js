8c57ac384d2dd13c809edd96660cc745674f97f9
bugdlr <htdelosreyes8@gmail.com>
2018-10-03 16:10:44 -0700

    cash register almost there

--- cashRegister.js
+++ cashRegister.js
@@ -1,52 +1,53 @@
 function checkCashRegister(price, cash, cid) {
-  let  change = cash - price;
-  let currency = [10000, 2000, 500, 100, 25, 10, 5, 1];
-  let response = {
-    status : "CLOSED",
-    change : [change]
-  }
+  let  change = (cash - price) * 100;
+  let currency = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
+  let changeArray = [];
 
+  let response = {
+    status : "",
+    change : []
+  }
 
   // fix floating points
   cid.forEach(el => el[1] = Math.round(el[1] * 100));
-  console.log(cid);
 
     // get total of cid
   let count = cid.reduce(function (accumulator, currentValue) {
-    return accumulator + currentValue[1] / 100;
+    return accumulator + currentValue[1];
   }, 0);
 
-  console.log(count);
-  console.log(change);
-
-
+console.log(change);
 
   // response if cid is less than change due
-  if (count < change) {
-    response.status = "INSUFFICIENT_FUNDS";
-    response.change = [];
+  for (let i = cid.length - 1; i > -1; i--) {
+    let changeGiven = 0;
+    while (cid[i][1] > 0 && change >= currency[i]) {
+      change -= currency[i];
+      cid[i][1] -= currency[i];
+      changeGiven += currency[i];
+      response.status = "OPEN";
+    }
+    if (changeGiven)
+      changeArray.push([cid[i][0], changeGiven]);
   }
 
+  if (change > 0) {
+      response.status = "INSUFFICIENT_FUNDS";
+  }
+
+  if (change == cid) {
+    response.status = "CLOSED";
+  }
+
+  changeArray.forEach(el => el[1] = (el[1] / 100));
+  response.change = changeArray;
+
   console.log(response.status);
-
-  // use a recursive function
-
-  //   while (change > 0) {
-  //     if (change > 100) {
-  //   response.change = ["ONE HUNDRED", (Math.floor(change / 100))];
-  //   return change = change - (100 * (Math.floor(change / 100)));
-  //   } if (change > 20) {
-  //   response.change = ["TWENTY", (Math.floor(change / 20))];
-  //   change = change - (20 * (Math.floor(change / 20)));
-  // }
-
-
-
+  console.log(change);
 
   // Here is your change, ma'am.
-  return response;
+   return response;
 }
-
 
 // Example cash-in-drawer array:
 // [["PENNY", 1.01],
@@ -59,4 +60,4 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-checkCashRegister(19.5, 200, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]);
